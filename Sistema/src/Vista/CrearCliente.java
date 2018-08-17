d3cb8751a28886fa6dcf0a862fb45a8d24610a6d
juan laso <juanflaso1@hotmail.com>
2018-08-16 21:53:01 -0500

    crear cliente hecho

---
+++ Sistema/src/Vista/CrearCliente.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import Controlador.ConexionPostgresql;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author User
+ */
+public class CrearCliente {
+    private VBox root;
+    private Stage crearCliente;
+    private HBox cedulaBox, nombreBox, telefonoBox;
+    private Label lblCedula, lblNombre, lblTelefono;
+    private TextField cedula, nombre, telefono;
+    private Button crear;
+    private ConexionPostgresql conexion;
+    
+    public CrearCliente(){
+        
+        setUp();
+        
+        
+    }
+    
+    public void setUp(){
+        root = new VBox();
+        root.setPadding(new Insets(20,20,20,20));
+        
+        crearCliente = new Stage();
+        crearCliente.setResizable(false);
+        crearCliente.setTitle("Ingresar Cliente");
+        crearCliente.initModality(Modality.WINDOW_MODAL);
+        
+        Scene scene = new Scene(root, 400, 400);
+        crearCliente.setScene(scene);
+        
+        setLabels();
+        setupTextFields();
+        setHBoxes();
+        
+        crear = new Button("Ingresar");
+        crear.setOnAction(e->ingresarCliente());
+        root.getChildren().add(crear);
+        
+        
+        root.setSpacing(20);
+        root.setAlignment(Pos.CENTER);
+        crearCliente.showAndWait();
+        
+    }
+    
+    public void setHBoxes(){
+        cedulaBox = new HBox();
+        cedulaBox.getChildren().addAll(lblCedula, cedula);
+        cedulaBox.setSpacing(24);
+        cedulaBox.setAlignment(Pos.CENTER);
+        
+        nombreBox = new HBox();
+        nombreBox.getChildren().addAll(lblNombre, nombre);
+        nombreBox.setSpacing(20);
+        nombreBox.setAlignment(Pos.CENTER);
+        
+        telefonoBox = new HBox();
+        telefonoBox.getChildren().addAll(lblTelefono, telefono);
+        telefonoBox.setSpacing(20);
+        telefonoBox.setAlignment(Pos.CENTER);
+        
+        root.getChildren().addAll(cedulaBox, nombreBox, telefonoBox);
+        
+    }
+    
+    private void setupTextFields(){
+        cedula = new TextField();
+        nombre = new TextField();
+        telefono = new TextField();
+    }
+    
+    public void setLabels() {
+        lblCedula = new Label("Cedula:");
+        lblNombre = new Label("Nombre:");
+        lblTelefono = new Label("Telefono:");
+    }
+    
+    public void ingresarCliente() {
+        conexion = new ConexionPostgresql();
+        try {
+            PreparedStatement st = conexion.getCnx().prepareStatement("INSERT INTO cliente (cedula, nombre, telefono, isvisible) VALUES (?, ?, ?, ?)");
+            st.setString(1, cedula.getText());
+            st.setString(2, nombre.getText());
+            st.setString(3, telefono.getText());
+            st.setBoolean(4, true);
+            st.executeUpdate();
+            st.close();
+            crearCliente.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CrearCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+}
--- Sistema/src/Vista/PanelVendedor.java
+++ Sistema/src/Vista/PanelVendedor.java
@@ -5,10 +5,15 @@
  */
 package Vista;
 
+import Modelo.Ctes;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 /**
  *
@@ -18,6 +23,7 @@ public class PanelVendedor {
     private VBox root;
     private HBox hbox;
     private Button cotizacion, cliente, venta;
+    private Label titulo;
     
     public PanelVendedor(){
         root = new VBox();
@@ -25,16 +31,48 @@ public class PanelVendedor {
     }
     
     private void setUp() {
-        hbox= new HBox();
+        root.setPadding(new Insets(20,20,20,20));
+        
+        titulo = new Label("Vendedor");
+	titulo.setFont(new Font("Comic Sans",40));
+        
         cotizacion = new Button("Cotizacion");
         cliente = new Button("Registrar cliente");
         venta = new Button("Venta");
-        hbox.getChildren().addAll(cotizacion,cliente,venta);
-        root.getChildren().add(hbox);
+        
+        setupFunctButtons();
+        
+        cotizacion.setPrefSize(Ctes.BUT_WIDTH, Ctes.BUT_HEIGHT);
+        cliente.setPrefSize(Ctes.BUT_WIDTH, Ctes.BUT_HEIGHT);
+        venta.setPrefSize(Ctes.BUT_WIDTH, Ctes.BUT_HEIGHT);
+        
+        root.setAlignment(Pos.CENTER);
+        root.setStyle("-fx-background-color: lavender");
+        root.getChildren().addAll(titulo);
+        root.getChildren().addAll(cotizacion,cliente,venta);
     }
 
     public VBox getRoot() {
         return root;
+    }
+    
+    public void showAlert(){
+	Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Oops!");
+        alert.setHeaderText("Not implemented yet.");
+        alert.setContentText("Sorry not sorry xd.");
+        alert.showAndWait();
+
+    }
+    
+    private void setupFunctButtons(){
+        cotizacion.setOnAction(e->showAlert());
+        cliente.setOnAction(e->crearCliente());
+        venta.setOnAction(e->showAlert());
+    }
+    
+    private void crearCliente() {
+        CrearCliente crear = new CrearCliente();
     }
     
 }
