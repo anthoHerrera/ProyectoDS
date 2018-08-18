9ac2590b3b2b35368cb74de76f8e74a06fe9e6e9
JuanJose <juajoflo@espol.edu.ec>
2018-08-18 16:25:55 -0500

    busqueda productos admin

--- Sistema/src/Vista/adminViews/PanelBusquedaArticulos.java
+++ Sistema/src/Vista/adminViews/PanelBusquedaArticulos.java
@@ -17,6 +17,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -32,11 +33,12 @@ import javafx.stage.Stage;
  * @author Anthony777
  */
 public final class PanelBusquedaArticulos {
+
     private final VBox root;
     private HBox hbox;
-    private Label titulo,ingreso;
-    private TextField texto;
-    private Button regresar,boton;
+    private Label titulo, ingreso;
+    private ComboBox<String> cb;
+    private Button regresar, boton;
     private ControllerAdmin controlador;
     private ObservableList<ArticuloLineaBlanca> articulos;
     private TableView<ArticuloLineaBlanca> tabla;
@@ -48,9 +50,9 @@ public final class PanelBusquedaArticulos {
         setUp();
         setUpButtons();
         setStage();
-        
+
     }
-    
+
     public void setStage() {
         articuloStage = new Stage();
         scene = new Scene(this.getRoot(), Ctes.APP_WIDHT, Ctes.APP_HEIGHT - 150);
@@ -61,136 +63,144 @@ public final class PanelBusquedaArticulos {
     }
 
     public void setUp() {
-        
+
         root.setPadding(new Insets(20, 20, 20, 20));
         controlador = new ControllerAdmin();
         tabla = new TableView<>();
-        
+
         hbox = new HBox(5);
         boton = new Button("Buscar");
-        texto = new TextField();
+        setUpComboBoxes();
         ingreso = new Label("Ingrese el producto a buscar");
-        hbox.getChildren().addAll(ingreso,texto,boton);
+        hbox.getChildren().addAll(ingreso, cb, boton);
         titulo = new Label("Productos");
         titulo.setFont(new Font("Cambria", 40));
-        generarTabla("select * from articulo");
+        generarTabla("select * from articulo where nombre like 'pan'");
         regresar = new Button("REGRESAR");
 
         regresar.setPrefSize(Ctes.BUT_WIDTH + 60, Ctes.BUT_HEIGHT - 40);
-        
+
         root.setStyle("-fx-background-color: lavender");
         root.setAlignment(Pos.CENTER);
-        root.getChildren().addAll(titulo,hbox,tabla,regresar);
+        root.getChildren().addAll(titulo, hbox,tabla);
 
     }
-    
-    private void generarTabla(String comando){
-        
+
+    public void setUpComboBoxes() {
+        cb = new ComboBox<>();
+        cb.getItems().addAll("Todos", "Lavadoras", "Cocinas", "Refrigeradoras");
+    }
+
+    private void generarTabla(String comando) {
+
         try {
             articulos = controlador.consultaArticulos(comando);
             if (articulos != null) {
                 tabla.setItems(articulos);
                 tabla.setEditable(false);
-                
+
                 TableColumn nombre = new TableColumn("Nombre");
                 nombre.setMinWidth(200);
                 nombre.setCellValueFactory(
                         new PropertyValueFactory<>("nombre"));
-                
+
                 TableColumn descripcion = new TableColumn("Descripcion");
                 descripcion.setMinWidth(200);
                 descripcion.setCellValueFactory(
                         new PropertyValueFactory<>("descripcion"));
-                
+
                 TableColumn marca = new TableColumn("Marca");
                 marca.setMinWidth(200);
                 marca.setCellValueFactory(
                         new PropertyValueFactory<>("marca"));
-                
+
                 TableColumn precio = new TableColumn("Precio");
                 precio.setMinWidth(200);
                 precio.setCellValueFactory(
                         new PropertyValueFactory<>("precio"));
-                
+
                 TableColumn tamano = new TableColumn("Tamano");
                 tamano.setMinWidth(200);
                 tamano.setCellValueFactory(
                         new PropertyValueFactory<>("tamano"));
-                
+
                 TableColumn potencia = new TableColumn("PotenciaTotal");
                 potencia.setMinWidth(200);
                 potencia.setCellValueFactory(
                         new PropertyValueFactory<>("potenciaTotal"));
-                
+
                 TableColumn inductores = new TableColumn("Inductores");
                 inductores.setMinWidth(200);
                 inductores.setCellValueFactory(
                         new PropertyValueFactory<>("inductores"));
-                
+
                 TableColumn voltaje = new TableColumn("Voltaje");
                 voltaje.setMinWidth(200);
                 voltaje.setCellValueFactory(
                         new PropertyValueFactory<>("voltaje"));
-                
+
                 TableColumn puertas = new TableColumn("Puertas");
                 puertas.setMinWidth(200);
                 puertas.setCellValueFactory(
                         new PropertyValueFactory<>("cantidadPuertas"));
-                
+
                 TableColumn capacidad = new TableColumn("Capacidad");
                 capacidad.setMinWidth(200);
                 capacidad.setCellValueFactory(
                         new PropertyValueFactory<>("capacidad"));
-                
+
                 TableColumn filtro = new TableColumn("Filtro Agua");
                 filtro.setMinWidth(200);
                 filtro.setCellValueFactory(
                         new PropertyValueFactory<>("filtroAgua"));
-                
+
                 TableColumn temp = new TableColumn("Niveles Temperatura");
                 temp.setMinWidth(200);
                 temp.setCellValueFactory(
                         new PropertyValueFactory<>("nivelesTemperatura"));
-                
-                tabla.getColumns().addAll(nombre,descripcion,marca,precio,tamano,potencia,inductores,voltaje,puertas,capacidad,filtro,temp);
-            }else {
+
+                tabla.getColumns().addAll(nombre, descripcion, marca, precio, tamano, potencia, inductores, voltaje, puertas, capacidad, filtro, temp);
+            } else {
                 showError();
             }
         } catch (SQLException ex) {
             Logger.getLogger(PanelBusquedaArticulos.class.getName()).log(Level.SEVERE, null, ex);
-        }   
+        }
     }
-            
+
     public VBox getRoot() {
         return root;
     }
-    
+
     private void setUpButtons() {
         regresar.setOnAction(e -> articuloStage.close());
-        boton.setOnAction(e-> manejarBuscar());
+        boton.setOnAction(e -> manejarBuscar());
     }
-    
-    private void manejarBuscar(){
-        if(!texto.getText().isEmpty()){
-            if(texto.getText().equals("cocina")){
-                root.getChildren().removeAll(titulo,hbox,tabla,regresar);
-                generarTabla("select * from articulo where nombre like 'Cocina%'");
-                root.getChildren().addAll(titulo,hbox,tabla,regresar);   
-            }
-            else if(texto.getText().equals("lavadora")){
-                root.getChildren().removeAll(titulo,hbox,tabla,regresar);
-                generarTabla("select * from articulo where nombre like 'Lavadora%'");
-                root.getChildren().addAll(titulo,hbox,tabla,regresar);   
-            }
-            else if(texto.getText().equals("refrigeradora")){
-                root.getChildren().removeAll(titulo,hbox,tabla,regresar);
-                generarTabla("select * from articulo where nombre like 'Refrigeradora%'");
-                root.getChildren().addAll(titulo,hbox,tabla,regresar);   
-            }
-            
+
+    private void manejarBuscar() {
+        if (cb.getValue().equals("Todos")) {
+            root.getChildren().removeAll(titulo, hbox, tabla);
+            generarTabla("select * from articulo");
+            root.getChildren().addAll(titulo, hbox, tabla);
         }
-        
+        else if (cb.getValue().equals("Cocinas")) {
+            root.getChildren().removeAll(titulo, hbox, tabla);
+            generarTabla("select * from articulo where nombre like 'Cocina%'");
+            root.getChildren().addAll(titulo, hbox, tabla);
+        } 
+        else if (cb.getValue().equals("Lavadoras")) {
+            root.getChildren().removeAll(titulo, hbox, tabla);
+            generarTabla("select * from articulo where nombre like 'Lavadora%'");
+            root.getChildren().addAll(titulo, hbox, tabla);
+        } 
+        else if (cb.getValue().equals("Refrigeradoras")) {
+            root.getChildren().removeAll(titulo, hbox, tabla);
+            generarTabla("select * from articulo where nombre like 'Refrigeradora%'");
+            root.getChildren().addAll(titulo, hbox, tabla);
+        }
+
     }
+
     private void showError() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
@@ -198,5 +208,5 @@ public final class PanelBusquedaArticulos {
         alert.setContentText("El sistema no ha podido mostrar los clientes");
         alert.showAndWait();
     }
-    
+
 }
