f2c032cb6046988808c011a03d391f61b5fcb5c9
Gabriel del Pino <gabparedes@hotmail.com>
2018-08-26 11:24:13 -0500

    mini update graph

--- Sistema/src/Strategy/TarjetaVISA.java
+++ Sistema/src/Strategy/TarjetaVISA.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Strategy;
 
 /**
@@ -11,7 +7,6 @@ package Strategy;
  */
 public class TarjetaVISA extends TarjetaCredito{
 
-	
 	
 	@Override
 	void validateCredentials() {
